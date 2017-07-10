1d112df58d691744d75852d6390a0d3456c4bb26
tannaurus <contacttannergill@gmail.com>
2017-07-10 11:04:33 -0700

    Added header component.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {AddPointInput} from '../AddPointInput/AddPointInput.js';
+import Header from '../Header/Header.js'
 
 export class App extends React.Component{
   render(){
-    return(<AddPointInput/>);
+    return(
+    <div>
+      <Header />
+      <AddPointInput/>
+    </div>);
   }
 }
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header(props){
+  return(
+    <header><h1>Graph.it</h1></header>
+  );
+}
