586542af5e2e61c8296e23f50eec41fa055aa589
tannaurus <contacttannergill@gmail.com>
2017-07-12 13:37:16 -0700

    Async function now works perfectly.

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import {Provider} from 'react-redux';
 import './index.css';
 console.log(store.getState())
 
+store.dispatch(handleGetByIdAndSetState('59668810aec7bb3dc1473f76'))
+
 ReactDOM.render(
   <Provider store={store}>
   <App />
