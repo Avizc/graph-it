da69afd28ff187a766304030e2218657f0ee0f68
tannaurus <contacttannergill@gmail.com>
2017-07-12 09:44:14 -0700

    Ran Prettier on the models file.

--- server/models.js
+++ server/models.js
@@ -1,32 +1,28 @@
 // 'use strict';
-const express=require('express');
-const mongoose=require('mongoose');
-const graphSchema=mongoose.Schema({
-    graphTitle:{type:String},
-    xLabel:{type:String},
-    yLabel:{type:String},
-    prefix:{type:String},
-    suffix:{type:String},
-    graphData:[
-        {index:Number},
-        {label:String},
-        {data:Number}
-    ]
+const express = require("express");
+const mongoose = require("mongoose");
+const graphSchema = mongoose.Schema({
+  graphTitle: { type: String },
+  xLabel: { type: String },
+  yLabel: { type: String },
+  prefix: { type: String },
+  suffix: { type: String },
+  graphData: [{ index: Number }, { label: String }, { data: Number }]
 });
 // const savedGraphSchema=mongoose.Schema({
 //     savedGraphTitle:{type:String,required:true}
 // });
-graphSchema.methods.apiRepr=function(){
-    return{
-        id:this._id,
-        graphTitle:this.graphTitle,
-        xLabel:this.xLabel,
-        yLabel:this.yLabel,
-        prefix:this.prefix,
-        suffix:this.suffix,
-        graphData:this.graphData
-    };
-}
-const Graphs=mongoose.model('Graphs',graphSchema);
+graphSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    graphTitle: this.graphTitle,
+    xLabel: this.xLabel,
+    yLabel: this.yLabel,
+    prefix: this.prefix,
+    suffix: this.suffix,
+    graphData: this.graphData
+  };
+};
+const Graphs = mongoose.model("Graphs", graphSchema);
 // const SavedGraphs=mongoose.model('SavedGraphs',savedGraphsSchema);
-module.exports={Graphs};
+module.exports = { Graphs };
