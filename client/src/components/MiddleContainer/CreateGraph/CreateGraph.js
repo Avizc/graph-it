1924ca27008dbace4718f9e2b2ff649a3fc84fcc
tannaurus <contacttannergill@gmail.com>
2017-07-13 12:19:01 -0700

    Added the feedback component. Updated the travis.yml

--- client/src/components/MiddleContainer/CreateGraph/CreateGraph.js
+++ client/src/components/MiddleContainer/CreateGraph/CreateGraph.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import './CreateGraph.css';
 import {handleNewGraph} from '../../../redux/actions';
-import {ToolTip} from '../ToolTip/ToolTip.js';
+import {ToolTip} from '../../ToolTip/ToolTip.js';
 
 export class CreateGraph extends React.Component{
   constructor(props){
