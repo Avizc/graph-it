dfd34fb926bf647b449fadebfc6064b4be47f214
Avi <avirzajac@gmail.com>
2017-07-13 15:47:01 -0400

    Setting up tests

--- client/src/redux/actions/index.test.js
+++ client/src/redux/actions/index.test.js
@@ -20,6 +20,46 @@ import{
 } from './actions'
 describe('handleNewData',()=>{
     it('Should return an action!',()=>{
+
+    })
+})
+describe('handleNewGraph',()=>{
+    it('Should return an action!',()=>{
+        
+    })
+})
+describe('toggleNewData',()=>{
+    it('Should return an action!',()=>{
+        
+    })
+})
+describe('resetGraph',()=>{
+    it('Should return an action!',()=>{
+        
+    })
+})
+describe('newGraph',()=>{
+    it('Should return an action!',()=>{
+        
+    })
+})
+describe('handleLoading',()=>{
+    it('Should return an action!',()=>{
+        
+    })
+})
+describe('handleError',()=>{
+    it('Should return an action!',()=>{
+        
+    })
+})
+describe('setGraphToState',()=>{
+    it('Should return an action!',()=>{
+        
+    })
+})
+describe('handleSavedUrl',()=>{
+    it('Should return an action!',()=>{
         
     })
 })
