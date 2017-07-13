7761073ef8f8d24c9c1ac4735b8dbd7600c0c838
tannaurus <contacttannergill@gmail.com>
2017-07-13 14:12:11 -0700

    Fixed feedback bug

--- client/src/components/MiddleContainer/GraphInfoContainer/GraphInfoContainer.js
+++ client/src/components/MiddleContainer/GraphInfoContainer/GraphInfoContainer.js
@@ -6,13 +6,14 @@ export class GraphInfoContainer extends React.Component{
 
   render(){
     if(this.props.url !== null){
-      this.url = <h4 className="graph-feedback">{this.props.feedback}<br/><strong>{this.props.url}</strong></h4>
+      this.url = <h4><strong>{this.props.url}</strong></h4>
     }else{
       this.url = undefined;
     }
     return(
       <section className="graph-info-container">
         <h3 className="graph-title">{this.props.graphTitle}</h3>
+        <h4 className="graph-feedback">{this.props.feedback}</h4>
         {this.url}
       </section>
     );
