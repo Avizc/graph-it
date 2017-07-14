ee6b17fa2f904e375a5c8c9d93aec8c7dc888a56
tannaurus <contacttannergill@gmail.com>
2017-07-14 09:41:05 -0700

    Added logic to not render the feedback message if linkView is true.

--- client/src/components/MiddleContainer/GraphInfoContainer/GraphInfoContainer.js
+++ client/src/components/MiddleContainer/GraphInfoContainer/GraphInfoContainer.js
@@ -5,25 +5,30 @@ import './GraphInfoContainer.css';
 export class GraphInfoContainer extends React.Component{
 
   render(){
-    if(this.props.url !== null){
-      this.url = <h4 className="graph-feedback"><strong>{this.props.url}</strong></h4>
+    if(this.props.linkView){
+      return null;
     }else{
-      this.url = undefined;
+      if(this.props.url !== null){
+        this.url = <h4 className="graph-feedback"><strong>{this.props.url}</strong></h4>
+      }else{
+        this.url = undefined;
+      }
+      return(
+        <section className="graph-info-container">
+          <h3 className="graph-title">{this.props.graphTitle}</h3>
+          <h4 className="graph-feedback">{this.props.feedback}</h4>
+          {this.url}
+        </section>
+      );
     }
-    return(
-      <section className="graph-info-container">
-        <h3 className="graph-title">{this.props.graphTitle}</h3>
-        <h4 className="graph-feedback">{this.props.feedback}</h4>
-        {this.url}
-      </section>
-    );
   }
 }
 
 const mapStateToProps = (state, props) => ({
   graphTitle: state.graphTitle,
   feedback: state.feedback,
-  url: state.url
+  url: state.url,
+  linkView: state.linkView
 })
 
 export default connect(mapStateToProps)(GraphInfoContainer) 
