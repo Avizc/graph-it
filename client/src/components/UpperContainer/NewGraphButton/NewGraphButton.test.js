53e9cb6c5155e3dcc90e3fd1f3d799fc785d212b
Avi <avirzajac@gmail.com>
2017-07-14 03:41:53 -0400

    Added initial smoke tests for all upper container items

---
+++ client/src/components/UpperContainer/NewGraphButton/NewGraphButton.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {shallow,mount} from 'enzyme';
+import NewGraphButton from './NewGraphButton.js';
+describe('<NewGraphButton />',()=>{
+    it('Render without crashing!',()=>{
+        shallow(<NewGraphButton />);
+    });
+});
---
+++ client/src/components/UpperContainer/ResetGraphButton/ResetGraphButton.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {shallow,mount} from 'enzyme';
+import ResetGraphButton from './ResetGraphButton.js';
+describe('<ResetGraphButton />',()=>{
+    it('Render without crashing!',()=>{
+        shallow(<ResetGraphButton />);
+    });
+});
