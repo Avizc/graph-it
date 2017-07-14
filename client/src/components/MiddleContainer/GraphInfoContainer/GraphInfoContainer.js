9d05c42724c0be4296c2df51055510a3056821fd
tannaurus <contacttannergill@gmail.com>
2017-07-14 09:52:30 -0700

    Changed the feedback to show to title of graph when viewed by a user with a link (bug) and (hopefully) made the link clickable.

--- client/src/components/MiddleContainer/GraphInfoContainer/GraphInfoContainer.js
+++ client/src/components/MiddleContainer/GraphInfoContainer/GraphInfoContainer.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import './GraphInfoContainer.css';
+import React from "react";
+import { connect } from "react-redux";
+import "./GraphInfoContainer.css";
 
-export class GraphInfoContainer extends React.Component{
-
-  render(){
-    if(this.props.linkView){
-      return null;
-    }else{
-      if(this.props.url !== null){
-        this.url = <h4 className="graph-feedback"><strong>{this.props.url}</strong></h4>
-      }else{
+export class GraphInfoContainer extends React.Component {
+  render() {
+    if (this.props.linkView) {
+      return (
+        <section className="graph-info-container">
+          <h3 className="graph-title">
+            {this.props.graphTitle}
+          </h3>
+        </section>
+      );
+    } else {
+      if (this.props.url !== null) {
+        this.url = (
+          <h4 className="graph-feedback">
+            <a href={this.props.url}>
+            <strong>
+              {this.props.url}
+            </strong>
+            </a>
+          </h4>
+        );
+      } else {
         this.url = undefined;
       }
-      return(
+      return (
         <section className="graph-info-container">
-          <h3 className="graph-title">{this.props.graphTitle}</h3>
-          <h4 className="graph-feedback">{this.props.feedback}</h4>
+          <h3 className="graph-title">
+            {this.props.graphTitle}
+          </h3>
+          <h4 className="graph-feedback">
+            {this.props.feedback}
+          </h4>
           {this.url}
         </section>
       );
@@ -29,6 +46,6 @@ const mapStateToProps = (state, props) => ({
   feedback: state.feedback,
   url: state.url,
   linkView: state.linkView
-})
+});
 
-export default connect(mapStateToProps)(GraphInfoContainer) 
+export default connect(mapStateToProps)(GraphInfoContainer);
