349208bb48b6b0d468d4cab41bf8207c2534ceb2
Avi <avirzajac@gmail.com>
2017-07-14 03:30:23 -0400

    Working on SaveButton tests

---
+++ client/src/components/LowerContainer/SaveButton/SaveButton.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {shallow,mount} from 'enzyme';
+import SaveButton from './SaveButton.js';
+describe('<SaveButton />',()=>{
+    it('Render without crashing!',()=>{
+        shallow(<SaveButton />);
+    });
+});
