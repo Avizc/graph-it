50541654532783581d587982f18ca97395a84cb3
Avi <avirzajac@gmail.com>
2017-07-14 02:50:57 -0400

    Issues with reducers tests

--- client/src/redux/reducers/index.test.js
+++ client/src/redux/reducers/index.test.js
@@ -17,11 +17,11 @@ import{
     handleError,
     setGraphToState,
     handleSavedUrl
-} from './index';
-import reducer from './index';
+} from './index.js';
+import reducer from './index.js';
 describe('Reducer',()=>{
     it('Testing initial state',()=>{
-        const state=reducer(undefined);//,{type:'__UNKNOWN'});
+        const state=reducer(undefined,{type:'__UNKNOWN'});
         expect(state.graphData).toEqual([]);
         expect(state.graphTitle).toEqual('');
         expect(state.prefix).toEqual('');
@@ -30,5 +30,22 @@ describe('Reducer',()=>{
         expect(state.url).toEqual(null);
         expect(state.newDataToggle).toEqual(false);
         expect(state.newGraphToggle).toEqual(true);
+    });
+    it('Should handle a new graph on handleNewGraph()',()=>{
+        let testState={
+            graphData: [],
+            graphTitle: 'Alice in Wonderland',
+            prefix: '$',
+            suffix: 'k',
+            feedback: "You have not saved your graph just yet. Click to save button when you've finished the graph.",
+            url: null,
+            newDataToggle: false,
+            newGraphToggle: true
+        };
+        const state=reducer(testState,handleNewGraph());
+        expect(state.graphTitle).toEqual('Rabbit Wonderland');
+        expect(state.prefix).toEqual('$');
+        expect(state.suffix).toEqual('m');
+        expect(state.newGraphToggle).toEqual(false);
     });
 });
