3b7ee3b78f6342a954203afc53e2563207ce10bb
Avi <avirzajac@gmail.com>
2017-07-14 03:25:35 -0400

    Working on AddDataInput tests

--- client/src/components/LowerContainer/AddDataContainer/AddDataButton/AddDataButton.test.js
+++ client/src/components/LowerContainer/AddDataContainer/AddDataButton/AddDataButton.test.js
@@ -5,4 +5,12 @@ describe('<AddPointButton />',()=>{
     it('Render without crashing!',()=>{
         shallow(<AddPointButton />);
     });
+    it('Renders the adding data button',()=>{
+        const callback=jest.fn();
+        const wrapper=shallow(<AddPointButton handleClick={callback} />);
+        wrapper.find('.dispatch').simulate('click',{
+            preventDefault(){}
+        });
+        expect(callback).toHaveBeenCalled();
+    });
 });
